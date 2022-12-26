c9004072e7caf8af08f89101bf97cfd4fbfec282
o.uagaz <o.uagaz@kmg.kz>
2022-12-26 23:03:11 +0600

    software folder added

--- Software/velvet_wifi/.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- /home/home/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /home/home/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /home/home/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/home/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qspi_qspi/include/sdkconfig.h
